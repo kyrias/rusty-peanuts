30b6bc2023604c6cac92a4fb9d6e84f583ca8299
Johannes Löthberg <johannes@kyriasis.com>
2020-11-22 16:22:33 +0100

    web: api: Add mising ? when getting photo by id
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- src/web/api/v1/mod.rs
+++ src/web/api/v1/mod.rs
@@ -36,7 +36,7 @@ async fn get_photo(req: Request<crate::State>) -> tide::Result<Response> {
     };
 
     let photo_id: i32 = req.param("photo_id")?.parse()?;
-    let res = match conn.get_photo_by_id(photo_id, published).await {
+    let res = match conn.get_photo_by_id(photo_id, published).await? {
         Some(photo) => Response::builder(tide::http::StatusCode::Ok)
             .body(tide::Body::from_json(&photo)?)
             .build(),
